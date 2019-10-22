812f5a60a2c160fd4fa19fa5e41568fcef08d3d2
Apple <apple@Apples-MacBook-Pro-3.local>
2019-10-22 10:20:45 +0530

    message

--- src/test/java/Pages/Start.java
+++ src/test/java/Pages/Start.java
@@ -2,18 +2,53 @@ package Pages;
 
 import Utils.CommonUtils;
 import Utils.Driver;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Start {
 
     WebDriver driver  ;
+    //WebDriverWait wait = new WebDriverWait(driver, 20);
 
     @Test
     public void openbrowser(){
-        this.driver = Driver.browser();
-        CommonUtils.launhBrowser(driver , "https://teamie-next.teamieapp.com/dash/#/classroom/118629/sections");
+        try {
+            this.driver = Driver.browser();
+            CommonUtils.launhBrowser(driver, "https://www.toolsqa.com/automation-practice-switch-windows/");
+            driver.manage().window().fullscreen();
+            String parentWindowhandle = driver.getWindowHandle();
+
+            System.out.println(parentWindowhandle);
+            Thread.sleep(4000);
+          //  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            JavascriptExecutor js = (JavascriptExecutor)driver;
+            js.executeScript("window.scrollBy(0,500)");
+            //((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+            Thread.sleep(4000);
+
+
+
+
+            WebElement test = driver.findElement(By.xpath("/html/body/div[1]/div[5]/div[2]/div/div/div[1]/div[2]/div/div/div/div/p[3]/button"));
+
+            test.click();
+
+
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+            driver.quit();
+        }
     }
-}
+
